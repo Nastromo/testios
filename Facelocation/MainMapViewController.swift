54b9fd3ea5494961058875878df397d6a6639382
Nastro Mo <nastromo@Sulako.local>
2018-03-03 22:48:09 +0200

    Added a menu

--- Facelocation/MainMapViewController.swift
+++ Facelocation/MainMapViewController.swift
@@ -3,6 +3,9 @@ import GoogleMaps
 
 class MainMapViewController: UIViewController {
     @IBOutlet weak var mapView: GMSMapView!
+    @IBOutlet weak var leftMenuConstrait: NSLayoutConstraint!
+    @IBOutlet weak var menuView: UIView!
+    var menuState = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -13,10 +16,29 @@ class MainMapViewController: UIViewController {
         imageView.image = image
         imageView.contentMode = .scaleAspectFit
         navigationItem.titleView = imageView
+        
+        menuView.layer.shadowOpacity = 0.5
+        menuView.layer.shadowRadius = 6
+        
     }
     
     func loadMap() {
         let camera = GMSCameraPosition.camera(withLatitude: 51.50, longitude: -0.076, zoom: 10.0)
         mapView.camera = camera
+    }
+    
+    @IBAction func showMenu(_ sender: Any) {
+        if (menuState) {
+            leftMenuConstrait.constant = -240;
+            UIView.animate(withDuration: 0.3, animations: {
+                self.view.layoutIfNeeded()
+            })
+        } else {
+            leftMenuConstrait.constant = 0;
+            UIView.animate(withDuration: 0.3, animations: {
+                self.view.layoutIfNeeded()
+            })
+        }
+        menuState = !menuState
     }
 }
