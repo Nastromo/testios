d1e2150782e6982796e1bc6d961b3022eeb86e4e
Nastro Mo <nastromo@Sulako.local>
2018-03-04 14:03:38 +0200

    My location and Search for places added

--- Facelocation/MainMapViewController.swift
+++ Facelocation/MainMapViewController.swift
@@ -1,15 +1,20 @@
 import UIKit
 import GoogleMaps
+import GooglePlaces
 
-class MainMapViewController: UIViewController {
+class MainMapViewController: UIViewController, GMSMapViewDelegate, CLLocationManagerDelegate, GMSAutocompleteViewControllerDelegate{
+
+    //OUTLETS
     @IBOutlet weak var mapView: GMSMapView!
     @IBOutlet weak var leftMenuConstrait: NSLayoutConstraint!
     @IBOutlet weak var menuView: UIView!
+    
+    //VARIABLES
     var menuState = false
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadMap()
         
         let imageView = UIImageView()
         let image = UIImage(named: "facelogo.png")
@@ -17,16 +22,28 @@ class MainMapViewController: UIViewController {
         imageView.contentMode = .scaleAspectFit
         navigationItem.titleView = imageView
         
+        mapView.isMyLocationEnabled = true
+        mapView.delegate = self
+        
+        //Location Manager code to fetch current location
+        self.locationManager.delegate = self
+        self.locationManager.startUpdatingLocation()
+        
         menuView.layer.shadowOpacity = 0.5
         menuView.layer.shadowRadius = 6
+    }
+    
+    //Animate camera on user current position
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        let camera = GMSCameraPosition.camera(withLatitude: (location?.coordinate.latitude)!, longitude:(location?.coordinate.longitude)!, zoom:16)
+        mapView.animate(to: camera)
         
+        //Finally stop updating location otherwise it will come again and again in this delegate
+        self.locationManager.stopUpdatingLocation()
     }
     
-    func loadMap() {
-        let camera = GMSCameraPosition.camera(withLatitude: 51.50, longitude: -0.076, zoom: 10.0)
-        mapView.camera = camera
-    }
-    
+    //App Menu
     @IBAction func showMenu(_ sender: Any) {
         if (menuState) {
             leftMenuConstrait.constant = -240;
@@ -41,4 +58,82 @@ class MainMapViewController: UIViewController {
         }
         menuState = !menuState
     }
+    
+    
+    //AutoComplete Google Search for Places
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        let camera = GMSCameraPosition.camera(withLatitude: place.coordinate.latitude, longitude: place.coordinate.longitude, zoom: 16)
+        self.mapView.camera = camera
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        print("ERROR!!!!!!! \(error)")
+    }
+    
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func searchForPlace(_ sender: Any) {
+        let autoCompleteController = GMSAutocompleteViewController()
+        autoCompleteController.delegate = self
+        
+        self.locationManager.startUpdatingLocation()
+        self.present(autoCompleteController, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
